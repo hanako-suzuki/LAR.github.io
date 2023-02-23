c8a973ab3e5ae98b73628b733534efa6b62f9af1
hanako-suzuki <100546035+hanako-suzuki@users.noreply.github.com>
2023-02-23 20:21:42 +0900

    test

--- text.js
+++ text.js
@@ -1,17 +1,3 @@
-// const Module = {
-//   onRuntimeInitialized() {
-//     const medias = {
-//       audio: false,
-//       video: {
-//         facingMode: 'user'
-//       }
-//     };
-//     const promise = navigator.mediaDevices.getUserMedia(medias);
-
-//     promise.then(successCallback).catch(errorCallback);
-//   }
-// };
-
 const medias = {
   audio: false,
   video: {
@@ -21,70 +7,46 @@ const medias = {
   }
 };
 const video  = document.getElementById("video");
-video.autoplay = true;
-video.muted = true;
-video.playsInline = true;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const promise = navigator.mediaDevices.getUserMedia(medias);
 
+let imgData;
+let data
+let ave;
+let img_diff;
+
+promise.then(successCallback)
+       .catch(errorCallback);
 
 function successCallback(stream) {
-  // const video = document.getElementById('video');
-  // video.autoplay = true;
-  // video.muted = true;
-  // video.playsInline = true;
-  // const canvas = document.getElementById('canvas');
-  // const ctx = canvas.getContext('2d');
-  // // test
-  const imgLength = 2;
-  const videoMatList = [];
-  const blackAndWhiteMatList = [];
-  const FPS = 30;
-  const diffMat = new cv.Mat(height, width, cv.CV_8UC1);
-
   video.srcObject = stream;
-
-  // video.oncanplay = () => {
-  //   const width = video.clientWidth/4; // 適当にリサイズ
-  //   const height = video.clientHeight/4; // 適当にリサイズ
-
-  //   for (let i = 0; i < imgLength; ++i) {
-  //     videoMatList.push(new cv.Mat(height, width, cv.CV_8UC4));
-  //     blackAndWhiteMatList.push(new cv.Mat(height, width, cv.CV_8UC1));
-  //   }
-
-  //   canvasList.forEach((canvas) => {
-  //     canvas.width = width;
-  //     canvas.height = height;
-  //   });
-
-  //   processVideo();
-
-  //   function processVideo() {
-  //     const begin = Date.now();
-
-  //     ctx.drawImage(video, 0, 0, width, height);
-
-  //     videoMatList[0].copyTo(videoMatList[1]); // 1フレーム前
-  //     videoMatList[0].data.set(ctx.getImageData(0, 0, width, height).data); // 現在
-  //     // cv.imshow('canvas', videoMatList[0]);
-
-  //     // グレースケールにする
-  //     cv.cvtColor(videoMatList[0], blackAndWhiteMatList[0], cv.COLOR_RGB2GRAY);
-  //     cv.cvtColor(videoMatList[1], blackAndWhiteMatList[1], cv.COLOR_RGB2GRAY);
-
-  //     diffMat = cv.absdiff(blackAndWhiteMatList[0], blackAndWhiteMatList[1]);
-  //     cv.imshow('canvas', diffMat);
-
-  //     const delay = 1000 / FPS - (Date.now() - begin);
-
-  //     setTimeout(processVideo, delay);
-  //   }
-  // };
-
-}
+  requestAnimationFrame(draw);
+};
 
 function errorCallback(err) {
   alert(err);
 };
+
+function draw() {
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.drawImage(video, 0, 0);
+
+  imgData = ctx.getImageData(0, 0, canvas.width,  canvas.height);
+    data = imgData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+      ave = (data[i + 0] + data[i + 1] + data[i + 2]) / 3;
+
+      data[i + 0] = 
+      data[i + 1] = 
+      data[i + 2] = (ave > 255 / 2) ? 255 : (ave > 255 / 4) ? 127 : 0;
+      data[i + 3] = 255;
+    }
+
+  ctx.putImageData(imgData, 0, 0);
+  requestAnimationFrame(draw);
+}
+
+
