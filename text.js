0e3ca017712a6e8e5e977e2376a990059bcb26b3
hanako-suzuki <100546035+hanako-suzuki@users.noreply.github.com>
2023-02-27 15:17:59 +0900

    print line on image

--- text.js
+++ text.js
@@ -68,6 +68,7 @@ function successCallback(stream) {
       H_inv = calc_H(R_pilot, G_pilot, B_pilot);
     }
     color(videoMatNow, H_inv);
+    cv.line(videoMatNow, (10,10),(10,11),(0,255,0),1);
 
     // ２値化
     // cv.cvtColor(videoMatNow, blackAndWhiteMatNow, cv.COLOR_RGB2GRAY);
@@ -185,46 +186,6 @@ function successCallback(stream) {
     }
     return H_inv;
   }
-  /* ここまで */
-  // video.oncanplay = () => {
-  //   const width = video.clientWidth;
-  //   const height = video.clientHeight;
-
-  //   const videoMatPre = new cv.Mat(height, width, cv.CV_8UC4);
-  //   const videoMatNow = new cv.Mat(height, width, cv.CV_8UC4);
-  //   const blackAndWhiteMatPre = new cv.Mat(height, width, cv.CV_8UC1);
-  //   const blackAndWhiteMatNow = new cv.Mat(height, width, cv.CV_8UC1);
-
-
-  //   canvasList.forEach((canvas) => {
-  //     canvas.width = width;
-  //     canvas.height = height;
-  //   });
-
-  //   processVideo();
-
-  //   function processVideo() {
-  //     const begin = Date.now();
-
-  //     ctx.drawImage(video, 0, 0, width, height);
-
-  //     videoMatPre.copyTo(videoMatNow);
-  //     videoMatNow.data.set(ctx.getImageData(0, 0, width, height).data);
-
-  //     // ２値化
-  //     cv.cvtColor(videoMatNow, blackAndWhiteMatNow, cv.COLOR_RGB2GRAY);
-  //     cv.cvtColor(videoMatPre, blackAndWhiteMatPre, cv.COLOR_RGB2GRAY);
-
-  //     // 差分取得
-  //     const diffMat = new cv.Mat(height, width, cv.CV_8UC1);
-  //     cv.absdiff(blackAndWhiteMatNow, blackAndWhiteMatPre, diffMat);
-  //     cv.imshow("canvas", diffMat);
-
-  //     const delay = 1000 / FPS - (Date.now() - begin);
-
-  //     setTimeout(processVideo, delay);
-  //   }
-  // };
 };
 
 function errorCallback(err) {
