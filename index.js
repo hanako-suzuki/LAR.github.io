769f9b9da3623a0b9948d26dd9bdabd066a77f99
hanako-suzuki <100546035+hanako-suzuki@users.noreply.github.com>
2023-03-03 17:37:34 +0900

    test hough

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ function successCallback(stream) {
 
       // 線分検出 Hough
       cv.Canny(diffMat, diffMat, 50, 200, 3);
-      // cv.HoughLines(diffMat, lines, 1, Math.PI / 180, 30, 0, 0, 0, Math.PI);
+      cv.HoughLines(diffMat, lines, 1, Math.PI / 180, 30, 0, 0, 0, Math.PI);
       // draw lines
       // for (let i = 0; i < lines.rows; ++i) {
       //   let rho = lines.data32F[i * 2];
