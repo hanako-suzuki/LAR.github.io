7e57d7839d4ada083f0d469d8f1e30b234765503
hanako-suzuki <100546035+hanako-suzuki@users.noreply.github.com>
2023-03-06 12:13:49 +0900

    test

--- index.js
+++ index.js
@@ -209,7 +209,8 @@ function successCallback(stream) {
         if(posLog.length == comp_length){
           let targetLines = posLog[comp_length-1].concat();
           let fuse_lines = fusion(targetLines);
-          // targetLines = integlate_lines(targetLines, threshold_size, comp_length);
+          fuse_lines = fusion(fuse_lines);
+          fuse_lines = integlate_lines(fuse_lines, threshold_size, comp_length);
           for(let i=0; i<fuse_lines.length; i++){
             if(fuse_lines[i][3] > comp_length * 0.8){
               cv.line(videoMatPre, fuse_lines[i][0], fuse_lines[i][1], colorRed);
