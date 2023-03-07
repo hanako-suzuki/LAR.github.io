5958315010759b710ae1c9d4f2494c91f1bbeed0
hanako-suzuki <100546035+hanako-suzuki@users.noreply.github.com>
2023-03-07 19:54:00 +0900

    change video and canvas size

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ function successCallback(stream) {
   const FPS = 30;
 
   /* ここから */
-  const width = canvas.width*1.5;
-  const height = canvas.height*4;
+  const width = video.clientWidth;
+  const height = video.clientHeight;
   const median = height/2;
 
   let videoMatPre = new cv.Mat(height, width, cv.CV_8UC4);
